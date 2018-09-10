95d31ea04f35ea5624cd593fdddf9acb92197a3b
Mason Banks <masonbanks@ymail.com>
2018-09-10 14:43:52 +0100

    working on the winning function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Button from './components/Button';
 import Alphabet from './components/Alphabet';
 import Display from './components/Display';
-import Score from './components/Score'
+import Streak from './components/Streak';
+import Score from './components/Score';
 import words from 'an-array-of-english-words';
 
 class App extends Component {
@@ -11,7 +12,8 @@ class App extends Component {
     alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     attempts: [],
     currentWord: "",
-    lives: 6
+    lives: 6,
+    streak: 0
   }
   render() {
     return (
@@ -19,6 +21,7 @@ class App extends Component {
         <h1>Hangman</h1>
         <Button getWord={this.getWord} />
         <Score lives={this.state.lives} />
+        <Streak streak={this.state.streak} />
         <Display currentWord={this.state.currentWord} attempts={this.state.attempts} lives={this.state.lives} />
         <Alphabet alphabet={this.state.alphabet} playersTurn={this.playersTurn} attempts={this.state.attempts} />
       </div>
@@ -29,7 +32,8 @@ class App extends Component {
     this.setState({
       attempts: [],
       currentWord: word,
-      lives: 6
+      lives: 6,
+      streak: 0
     })
     // console.log(word)
   }
@@ -55,10 +59,15 @@ class App extends Component {
     if (!this.state.currentWord.split('').includes(value)) {
       this.playerLives()
     }
-    this.didYouWin()
+    this.checkWord
   }
-  didYouWin = () => {
-
+  didYouWin = (currentWord) => {
+    if (this.state.attempts.every(this.checkWord)) {
+      console.log('hello')
+    }
+  }
+  checkWord = () => {
+    return this.state.attempts.includes(this.state.currentWord.split(''))
   }
 }
 
---
+++ src/components/Streak.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Streak({ streak }) {
+  return (
+    <div className="streak">winning streak: {streak}</div>
+  )
+} 
